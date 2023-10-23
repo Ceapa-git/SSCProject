6fd2b8ccf61f553e237f4e5da7b282c9323d0616
Bugnariu Dan <dan.bugnariu0902@gmail.com>
2023-10-23 22:38:30 +0300

    added tree

---
+++ src/main/java/org/ssc/gui/MainController.java
@@ -0,0 +1,10 @@
+package org.ssc.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+public class MainController {
+}
